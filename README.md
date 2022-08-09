43d9b1bc0108573f1a7112ea265afe0f116153b5
Bilal Güler <guleerbilal@gmail.com>
2022-08-10 00:49:15 +0300

    First Commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# MERHABA PROGRAMIMA HOŞ GELDİNİZ
+
+* [**Patika Profil Linkim**](https://app.patika.dev/guleerbilal)
+
+* Bu programım kullanıcın girdiği sayısının harmonik serisini buluyor.
+
+## *Kodlarım*
+
+*![kodlarım](img/kod.PNG)
+
+*![output](img/output.PNG)
+
---
+++ src/Main.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int sayi;
+        double top = 0.0;
+
+        System.out.print("Harmanik serisini bulmak istediginiz sayi: ");
+        sayi = scan.nextInt();
+
+        for (double i = 1; i <= sayi; i++) {
+            top += (1 / i);
+        }
+        System.out.println("Harmonik seri: " + top);
+    }
+}
